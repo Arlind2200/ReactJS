50f2bf8da13f9ccdfc8828c2e8967cf9fcb1a32e
Arlind2001 <arlindhajrullahu00@gmail.com>
2023-06-24 14:46:00 +0200

    taska e javascript

---
+++ monorepo/javascript/index.js
@@ -0,0 +1,29 @@
+function shuffleValues() {
+    let form = document.getElementById("myForm");
+    let checkboxes = Array.from(document.querySelectorAll(".checkbox-label"));
+    checkboxes.sort(() => Math.random() - 0.5);
+    console.log(checkboxes);
+    checkboxes.forEach(checkbox => {
+      form.appendChild(checkbox);
+    });
+  }
+
+  function changeValues() {
+    let checkboxes = document.getElementsByName("checkbox");
+    let values = ["vlera 1", "vlera 2", "vlera 3", "vlera 4"];
+
+    checkboxes.forEach((checkbox, index) => {
+        checkbox.value = values[index];
+      });
+  }
+
+  function showSelectedValues() {
+    let checkboxes = document.getElementsByName("checkbox");
+    let selectedValues = [];
+    checkboxes.forEach(checkbox => {
+      if (checkbox.checked) {
+        selectedValues.push(checkbox.value);
+      }
+    });
+    alert("Selected Values: " + selectedValues);
+  } 
